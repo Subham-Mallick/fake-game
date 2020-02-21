85cacedcb2a5d521857bdc4b95717075cd88a91c
Subham Mallick <subham2mallick.com>
2020-02-21 12:49:00 +0530

    Change content

--- src/main/java/com/psych/game/HelloWorldController.java
+++ src/main/java/com/psych/game/HelloWorldController.java
@@ -10,7 +10,6 @@ public class HelloWorldController {
 
     @GetMapping("/")
     public String hello(){
-        return "My love, I will hold you when you are down, I will see you attractive even in your worst look, my love, I will be there for you at your worst moment. I will be by your side when the world denies you, will always be by your side, by you I will be!\n" +
-                "\n -For My Lovely Lady, Anandita";
+        return "Hello Subham! Its Heroku site deployment working fine";
     }
 }
